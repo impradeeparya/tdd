3ce3088587ef1f55e74cbc7eae96641da956d792
Pradeep Arya <impradeeparya@gmail.com>
2015-08-15 16:19:53 -0700

    change my ad display controller

--- src/main/webapp/resources/js/app/controllers/ad/controller.js
+++ src/main/webapp/resources/js/app/controllers/ad/controller.js
@@ -1,129 +1,143 @@
 /**
  * Created by Pradeep Arya on 7/5/2015.
  */
-angular.module('hr.ad.controller', [ 'hr.ad.services' ]).controller(
-		'AdController',
-		function($scope, $routeParams, $location, AdServices, UtilServices,
-				ngDialog) {
-			$scope.adsList = null;
-			$scope.myAds = null;
-			if ($routeParams.zoneId) {
-				AdServices.getAllZoneAds($routeParams.zoneId).success(
-						function(data) {
-							$scope.adsList = data;
-						})
+angular
+		.module('hr.ad.controller', [ 'hr.ad.services' ])
+		.controller(
+				'AdController',
+				function($scope, $routeParams, $location, AdServices,
+						UtilServices, ngDialog) {
+					$scope.adsList = null;
+					$scope.myAds = null;
+					if ($routeParams.zoneId) {
+						AdServices.getAllZoneAds($routeParams.zoneId).success(
+								function(data) {
+									$scope.adsList = data;
+								})
 
-				UtilServices.getLocalitiesByZone($routeParams.zoneId).success(
-						function(data) {
-							$scope.localities = data;
-						});
-			}
-
-			if ($routeParams.cityId) {
-				AdServices.getAllCityAds($routeParams.cityId).success(
-						function(data) {
-							$scope.adsList = data;
-						})
-
-				UtilServices.getLocalitiesByCity($routeParams.cityId).success(
-						function(data) {
-							$scope.localities = data;
-						});
-			}
-			$scope.updateAdStatus = function(adId) {
-				AdServices.updateAdStatus(adId).success(function(res) {
-					if (true == res) {
-						currentUserAds();
+						UtilServices.getLocalitiesByZone($routeParams.zoneId)
+								.success(function(data) {
+									$scope.localities = data;
+								});
 					}
-				});
-			}
 
-			$scope.contactInformationPopup = function(advertiser) {
-				$scope.popupInfo = advertiser;
-				ngDialog.open({
-					template : 'views/ad/_contactInfoPopup.html',
-					className : 'ngdialog-theme-default',
-					showClose : true,
-					closeByEscape : true,
-					closeByDocument : true,
-					name : 'Contact Information',
-					scope : $scope
-				});
-			}
+					if ($routeParams.cityId) {
+						AdServices.getAllCityAds($routeParams.cityId).success(
+								function(data) {
+									$scope.adsList = data;
+								})
 
-		}).controller('MyAdsController', function($scope, AdServices) {
+						UtilServices.getLocalitiesByCity($routeParams.cityId)
+								.success(function(data) {
+									$scope.localities = data;
+								});
+					}
 
-	AdServices.getCurrentUserAds().success(function(data) {
-		$scope.myAds = data;
-	})
-}).controller(
-		'PostAdController',
-		function($scope, UtilServices, AdServices) {
-
-			$scope.ad = {};
-			$scope.roomImages;
-
-			$scope.city = {};
-			$scope.zone = {};
-			$scope.locality = {};
-
-			$scope.showLocation = false;
-			$scope.showZone = false;
-
-			UtilServices.getAllCities().then(function(res) {
-				$scope.cities = res.data;
-			});
-
-			$scope.loadZones = function() {
-				if (!$scope.showZone) {
-					$scope.showZone = true;
-				}
-				if ($scope.city) {
-					UtilServices.getZonesByCity($scope.city.id).then(
-							function(res) {
-								$scope.zones = res.data
-							})
-				} else {
-					$scope.zones = {};
-					$scope.showZone = false;
-				}
-				$scope.zone = {};
-				$scope.localities = {};
-				$scope.locality = {};
-				$scope.showLocation = false;
-			};
-
-			$scope.loadLocalities = function() {
-				if (!$scope.showLocation) {
-					$scope.showLocation = true;
-				}
-				if ($scope.zone) {
-					UtilServices.getLocalitiesByZone($scope.zone.id).then(
-							function(res) {
-								$scope.localities = res.data
-							})
-				} else {
-					$scope.localities = {};
-					$scope.showLocation = false;
-				}
-				$scope.locality = {};
-			};
-
-			$scope.postAd = function() {
-				$scope.ad.locality = $scope.locality;
-				var images = new FormData();
-				angular.forEach($scope.roomImages, function(image) {
-					images.append(image.name, image);
-				});
-				AdServices.uploadRoomImages(images).success(
-						function(res) {
-							$scope.imageIds = res;
-							AdServices.postAd($scope.ad, $scope.imageIds)
-									.success(function(data) {
-										console.log(data);
-									})
+					$scope.contactInformationPopup = function(advertiser) {
+						$scope.popupInfo = advertiser;
+						ngDialog.open({
+							template : 'views/ad/_contactInfoPopup.html',
+							className : 'ngdialog-theme-default',
+							showClose : true,
+							closeByEscape : true,
+							closeByDocument : true,
+							name : 'Contact Information',
+							scope : $scope
 						});
+					}
 
-			};
+				})
+		.controller(
+				'MyAdsController',
+				function($scope, AdServices) {
 
-		})
+					$scope.myAds = null;
+
+					AdServices.getCurrentUserAds().success(function(data) {
+						$scope.myAds = data;
+					})
+
+					$scope.updateAdStatus = function(adId) {
+						AdServices
+								.updateAdStatus(adId)
+								.success(
+										function(res) {
+											if (true == res) {
+												for (var index = 0; index < $scope.myAds.length; index++) {
+													if ($scope.myAds[index].id == adId) {
+														$scope.myAds[index].isActive = !$scope.myAds[index].isActive;
+														break;
+													}
+												}
+											}
+										});
+					}
+				}).controller(
+				'PostAdController',
+				function($scope, UtilServices, AdServices) {
+
+					$scope.ad = {};
+					$scope.roomImages;
+
+					$scope.city = {};
+					$scope.zone = {};
+					$scope.locality = {};
+
+					$scope.showLocation = false;
+					$scope.showZone = false;
+
+					UtilServices.getAllCities().then(function(res) {
+						$scope.cities = res.data;
+					});
+
+					$scope.loadZones = function() {
+						if (!$scope.showZone) {
+							$scope.showZone = true;
+						}
+						if ($scope.city) {
+							UtilServices.getZonesByCity($scope.city.id).then(
+									function(res) {
+										$scope.zones = res.data
+									})
+						} else {
+							$scope.zones = {};
+							$scope.showZone = false;
+						}
+						$scope.zone = {};
+						$scope.localities = {};
+						$scope.locality = {};
+						$scope.showLocation = false;
+					};
+
+					$scope.loadLocalities = function() {
+						if (!$scope.showLocation) {
+							$scope.showLocation = true;
+						}
+						if ($scope.zone) {
+							UtilServices.getLocalitiesByZone($scope.zone.id)
+									.then(function(res) {
+										$scope.localities = res.data
+									})
+						} else {
+							$scope.localities = {};
+							$scope.showLocation = false;
+						}
+						$scope.locality = {};
+					};
+
+					$scope.postAd = function() {
+						$scope.ad.locality = $scope.locality;
+						console.log($scope.ad)
+						var images = new FormData();
+						/*
+						 * angular.forEach($scope.roomImages, function(image) {
+						 * images.append(image.name, image); });
+						 * AdServices.uploadRoomImages(images).success(
+						 * function(res) { $scope.imageIds = res;
+						 * AdServices.postAd($scope.ad, $scope.imageIds)
+						 * .success(function(data) { console.log(data); }) });
+						 */
+
+					};
+
+				})
