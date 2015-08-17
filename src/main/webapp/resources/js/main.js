af500513e501430492559339fd2a33053729813c
Pradeep Arya <impradeeparya@gmail.com>
2015-08-16 22:34:45 -0700

    profile logo changes

--- src/main/webapp/resources/js/main.js
+++ src/main/webapp/resources/js/main.js
@@ -3,8 +3,7 @@
  */
 
 $(document).ready(function() {
-	$(".btn-pref .btn ").click(function() {
-		alert('clicked');
+	$(".btn-pref .btn").click(function() {
 		$(".btn-pref .btn").removeClass("btn-primary").addClass("btn-default");
 		$(this).removeClass("btn-default").addClass("btn-primary");
 	});
